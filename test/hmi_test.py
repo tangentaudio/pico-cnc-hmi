8c0affc45ae5a839d4c5268f30405df6c968a8de
Steve Richardson <tangentaudio@gmail.com>
2025-02-25 20:30:24 -0500

    hal side test

--- test/hmi_test.py
+++ test/hmi_test.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-# Install python3 HID package https://pypi.org/project/hid/
 import hid
+import hal
+import struct
+import time
+
+HAL = hal.component("hmi_test")
 
 def hex_dump(data, length=16):
     if isinstance(data, str):
@@ -11,19 +15,28 @@ def hex_dump(data, length=16):
         hex_string = ' '.join(f'{byte:02x}' for byte in chunk)
         print(f'{hex_string:<{length*3-1}} ')
 
+HAL.newpin('knob.0.value', hal.HAL_S32, hal.HAL_OUT)
+HAL.newpin('knob.0.button', hal.HAL_BIT, hal.HAL_OUT)
+HAL.newpin('knob.1.value', hal.HAL_S32, hal.HAL_OUT)
+HAL.newpin('knob.1.button', hal.HAL_BIT, hal.HAL_OUT)
 
-# default is TinyUSB (0xcafe), Adafruit (0x239a), RaspberryPi (0x2e8a), Espressif (0x303a) VID
-USB_VID = (0xcafe, 0x239a, 0x2e8a, 0x303a)
+HAL.ready()
 
-print("VID list: " + ", ".join('%02x' % v for v in USB_VID))
+for devinfo in hid.enumerate(0xcafe):
+    vid = devinfo['vendor_id']
+    pid = devinfo['product_id']
+    print("Found device: VID=%04x PID=%04x" % (vid, pid))
+    dev = hid.Device(vid, pid)
+    if dev:
+        while True:
+            str_out = b'\x00'
+            buf = dev.read(64, 10)
+            if len(buf) == 64:
+                pkt = struct.unpack('<iiBB', buf[0:10])
+                #print(buf)
+                hex_dump(buf, 64)
+                HAL['knob.0.value'] = pkt[0]
+                HAL['knob.1.value'] = pkt[1]
+                HAL['knob.0.button'] = not pkt[2]
+                HAL['knob.1.button'] = not pkt[3]
 
-for vid in USB_VID:
-    for dict in hid.enumerate(vid):
-        print(dict)
-        dev = hid.Device(dict['vendor_id'], dict['product_id'])
-        if dev:
-            while True:
-                str_out = b'\x00'
-                buf = dev.read(64, 10)
-                if len(buf) > 0:
-                    hex_dump(buf, 64)
